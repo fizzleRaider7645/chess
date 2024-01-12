ef71b193daf9db1eac3342684bce387f7c7a8afb
Douglas Uretsky <douglasuretsky@gmail.com>
2024-01-12 18:41:10 -0500

    fleshing out how to move pieces

--- src/componets/Board/Square/index.tsx
+++ src/componets/Board/Square/index.tsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { Context } from "../Context";
 import { PieceToRender } from "./subcomponents";
 import { determineIsDark } from "./helpers";
+import { PieceColors, PieceLabels } from "../../Piece/types";
 
 const Square = ({ columnIndex, rowIndex, piece }: SquareProps): JSX.Element => {
   const { chessBoard, setChessBoard, selectedPiece, setSelectedPiece } =
@@ -12,11 +13,21 @@ const Square = ({ columnIndex, rowIndex, piece }: SquareProps): JSX.Element => {
   const isDark = determineIsDark(columnIndex, rowIndex);
 
   const onClickHandler = () => {
-    // setSelectedPiece();
+    setSelectedPiece(piece);
+    // const pieceToMove = {
+    //   label: PieceLabels.Pawn,
+    //   color: PieceColors.White,
+    //   position: { row: rowIndex, col: columnIndex },
+    // };
+    console.log(
+      `columnIndex: ${columnIndex}`,
+      `rowIndex: ${rowIndex}`,
+      selectedPiece
+    );
   };
 
   return (
-    <SquareElement $isDark={isDark} onClick={() => onClickHandler}>
+    <SquareElement $isDark={isDark} onClick={onClickHandler}>
       <PieceToRender
         piece={piece}
         rowIndex={rowIndex}
