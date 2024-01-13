6833cbf8b43791327dee1fd772259844d462c6e9
Douglas Uretsky <douglasuretsky@gmail.com>
2024-01-13 11:30:00 -0500

    we're moving

--- src/componets/Board/Square/index.tsx
+++ src/componets/Board/Square/index.tsx
@@ -4,8 +4,6 @@ import { useContext } from "react";
 import { Context } from "../Context";
 import { PieceToRender } from "./subcomponents";
 import { determineIsDark } from "./helpers";
-// import { PieceColors, PieceLabels } from "../../Piece/types";
-import { Board } from "../types";
 
 const Square = ({ columnIndex, rowIndex, piece }: SquareProps): JSX.Element => {
   const { chessBoard, setChessBoard, selectedPiece, setSelectedPiece } =
@@ -14,23 +12,36 @@ const Square = ({ columnIndex, rowIndex, piece }: SquareProps): JSX.Element => {
   const isDark = determineIsDark(columnIndex, rowIndex);
 
   const onClickHandler = () => {
-    const newBoard: Board = [...chessBoard];
+    // Clone the current chess board
+    const newBoard = chessBoard.map((row) => [...row]);
 
-    setSelectedPiece(piece);
-    // const pieceToMove = {
-    //   label: PieceLabels.Pawn,
-    //   color: PieceColors.White,
-    //   position: { row: rowIndex, col: columnIndex },
-    // };
-    console.log(
-      `columnIndex: ${columnIndex}`,
-      `rowIndex: ${rowIndex}`,
-      selectedPiece
-    );
+    // If no piece is currently selected and the clicked square has a piece, select the piece
+    if (!selectedPiece && piece) {
+      setSelectedPiece({
+        ...piece,
+        position: { row: rowIndex, col: columnIndex },
+      });
+    }
+    // If a piece is selected and the clicked square is a valid destination, move the piece
+    else if (
+      selectedPiece //&&
+      // isValidDestination(selectedPiece, rowIndex, columnIndex, chessBoard)
+    ) {
+      const {
+        position: { row, col },
+      } = selectedPiece;
+      // Move the piece to the new location
+      newBoard[rowIndex][columnIndex] = selectedPiece;
 
-    newBoard[rowIndex][columnIndex] = selectedPiece;
+      // Remove the piece from its original location
+      newBoard[row][col] = null;
 
-    // setChessBoard(newBoard);
+      // Update the board state
+      setChessBoard(newBoard);
+
+      // Deselect the piece after moving
+      setSelectedPiece(null);
+    }
   };
 
   return (
