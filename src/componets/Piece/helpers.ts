e366a2cfcb1b8bdfafda74a04ca854dff5f33d0b
Douglas Uretsky <douglasuretsky@gmail.com>
2024-01-15 16:26:07 -0500

    add generateUniqueId just in case

--- src/componets/Piece/helpers.ts
+++ src/componets/Piece/helpers.ts
@@ -1,5 +1,9 @@
 import { DetermineIsSelectedProps } from "./types";
 
+export const generateUniqueId = () => {
+  return Math.random().toString(36).substring(7);
+};
+
 export const determineIsSelected = ({
   selectedPiece,
   rowIndex,
