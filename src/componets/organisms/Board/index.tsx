51a570539620dc33ca3b6191c801cd0b2ff6a914
Douglas Uretsky <douglasuretsky@gmail.com>
2024-01-26 17:51:16 -0500

    remove reset

--- src/componets/organisms/Board/index.tsx
+++ src/componets/organisms/Board/index.tsx
@@ -2,20 +2,15 @@ import React from "react";
 import Row from "../../molecules/Row";
 import { Container, ParentContainer } from "./subcomponents";
 import { useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../../../store";
-import { useDispatch } from "react-redux";
-import { attemptRevert } from "../../../features/board/middleWare";
+import { RootState } from "../../../store";
 
 const Board: React.FC = () => {
   const chessBoard = useSelector(
     ({ boardState: { board } }: RootState) => board
   );
 
-  const dispatch = useDispatch<AppDispatch>();
-
   return (
     <ParentContainer>
-      <button onClick={() => dispatch(attemptRevert())}>Reset</button>
       <Container>
         {chessBoard.map((_, rowIndex) => (
           <Row
