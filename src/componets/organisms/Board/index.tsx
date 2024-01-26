48620782a00b93dc22d4f907c29e2b3b3e279939
Douglas Uretsky <douglasuretsky@gmail.com>
2024-01-25 19:50:57 -0500

    closer

--- src/componets/organisms/Board/index.tsx
+++ src/componets/organisms/Board/index.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import Row from "../../molecules/Row";
 import { Container, ParentContainer } from "./subcomponents";
 import { useSelector } from "react-redux";
-import { RootState } from "../../../store";
-
+import { AppDispatch, RootState } from "../../../store";
+import { useDispatch } from "react-redux";
+import { revert } from "../../../features/board/boardSlice";
 const Board: React.FC = () => {
   const chessBoard = useSelector(
     ({ boardState: { board } }: RootState) => board
   );
 
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <ParentContainer>
+      <button onClick={() => dispatch(revert())}>Reset</button>
       <Container>
         {chessBoard.map((_, rowIndex) => (
           <Row
--- src/features/board/boardSlice.ts
+++ src/features/board/boardSlice.ts
@@ -7,38 +7,36 @@ const initialState: BoardState = {
   board: initialBoardSetup(),
   selectedSquare: null,
   selectedPiece: null,
-  frames: [],
+  gameFrames: [],
 };
 
 const boardStateSlice = createSlice({
   name: "boardState",
   initialState,
   reducers: {
-    // make reducers pure / only input
     movePiece: (state, { payload }: PayloadAction<MoveActionType>) => {
       const { currentPosition, nextPosition } = payload;
-      // Move the selected piece to the next position
       state.board[nextPosition.row][nextPosition.col] = state.selectedPiece;
 
-      // Clear the starting position
       state.board[currentPosition.row][currentPosition.col] = null;
 
-      // Add last move to frames
-      [...state.frames, state.board];
+      state.gameFrames = [...state.gameFrames, state.board];
 
-      // Optionally, reset selectedPiece and selectedSquare if you want to deselect after moving
       state.selectedPiece = null;
       state.selectedSquare = null;
     },
     revert: (state) => {
-      const { frames } = state;
-      const lastFrame = frames.pop();
-
-      state.board = lastFrame;
+      if (state.gameFrames.length > 1) {
+        const previousFrameIndex = state.gameFrames.length - 2;
+        const previousBoard = state.gameFrames[previousFrameIndex];
+        state.board = previousBoard;
+        state.gameFrames.pop();
+        state.selectedSquare = null;
+        state.selectedPiece = null;
+      }
     },
     updateSelectedSquare: (state, { payload }: PayloadAction<Position>) => {
       state.selectedSquare = payload;
-      // Ensure that the selected piece is updated based on the new selected square
       state.selectedPiece = state.board[payload.row][payload.col] ?? null;
     },
   },
